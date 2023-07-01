2a025b3881b1e6ad96e489af00e40d9a78ed10ef
Zack F <zackaryfotheringham@hotmail.com>
2023-07-01 07:25:25 -0400

    add initial application page

---
+++ pages/applications/[company]-[position]-[id].vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ApplicationPage } from "../../utils/customTypes";
+const props = defineProps<{ application: ApplicationPage }>();
+const {
+  company,
+  location,
+  position,
+  status,
+  updatedAt,
+  summary,
+  posting,
+  salary,
+  benefits,
+  notes,
+  createdAt,
+  appliedAt,
+  interviewedAt,
+  deniedAt,
+} = props.application;
+</script>
+
+<template>
+  <main class="px-2 py-6 md:px-4 md:py-8">
+    <header>
+      <h1>{{ position }}</h1>
+      <div class="flex justify-between">
+        <div>
+          <span>{{ company.name }}</span>
+          <span>{{ location }}</span>
+        </div>
+        <span>Updated: {{ updatedAt }}</span>
+      </div>
+    </header>
+    <section></section>
+  </main>
+</template>
